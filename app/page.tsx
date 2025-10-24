191df474addc71c8469dc45896d9a9cb64918955
graBro1 <grabtc7@gmail.com>
2025-10-23 20:32:04 -0700

    Create initial main page design

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main>
+      <div className='flex mx-auto bg-gray-950 border-b border-b-gray-900 h-18 items-center'>
+        <h1 className='mx-auto text-xl font-bold'>PDF Searcher</h1>
+      </div>
+      <div className="h-500 p-10">
+        <div>
+          <form className="mx-auto">
+            <div className="mx-auto flex flex-col items-center">
+              <input type="text" id="courseName" className="p-4 rounded-lg bg-gray-900 m-2 h-10 w-200 border-gray-800 border" placeholder="Enter Course Name"/>
+              <textarea id="courseDescription" className="p-4 rounded-lg bg-gray-900 m-2 h-50 w-200 border border-gray-800 resize-none" placeholder="Enter Course Description"></textarea>
+            </div>
+            <div className="mx-auto w-max p-4 flex flex-col">
+              <label className="text-center font-bold text-gray-400 p-2">Upload Course Catalog</label>
+              <input type="file" id="courseCatalog" className="block border border-gray-700 rounded-lg file:bg-gray-700 file:px-3 file:py-2 file:me-3 file:cursor-pointer file:hover:bg-gray-800"/>
+            </div>
+            <button type="submit" className="px-5 py-1.5 m-6 rounded-lg bg-gray-800 mx-auto block text-center cursor-pointer border-gray-700 border text-lg">Submit</button>
+
+          </form>
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
