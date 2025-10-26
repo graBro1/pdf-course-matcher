1e536dbec0c82e17ba4259c5f76a955c7130d069
graBro1 <grabtc7@gmail.com>
2025-10-25 22:52:50 -0700

    Allow form content & pdf uploads to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,5 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException, Form, File
+from typing import Annotated
+
 
 app = FastAPI()
+
+@app.post("/compare")
+async def compare(
+    name: Annotated[str, Form(...)],
+    description: Annotated[str, Form(...)],
+    catalog: UploadFile = File(...)
+    ):
+
+    if catalog.content_type != "application/pdf":
+        raise HTTPException(status_code=422, detail="File not found.")
+    
+    
+    return
+
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { compare } from "@/actions/actions";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,20 +6,38 @@ export default function Home() {
     <main>
       <div className="h-screen p-10">
         <div>
-          <form className="mx-auto">
+          <form action={compare} className="mx-auto">
             <label className="text-center block font-bold text-gray-400 p-2">Enter Course Information</label>
             <div className="mx-auto flex flex-col items-center">
-              <input type="text" id="courseName" className="p-4 rounded-lg bg-gray-900 m-2 h-10 w-200 border-gray-800 border" placeholder="Enter Course Name"/>
-              <textarea id="courseDescription" className="p-4 rounded-lg bg-gray-900 m-2 h-50 w-200 border border-gray-800 resize-none" placeholder="Enter Course Description"></textarea>
+              <input 
+              type="text" 
+              id="courseName" 
+              name="name"
+              className="p-4 rounded-lg bg-gray-900 m-2 h-10 w-200 border-gray-800 border" 
+              placeholder="Enter Course Name"
+              />
+              <textarea 
+              id="courseDescription"
+              name="description"
+              className="p-4 rounded-lg bg-gray-900 m-2 h-50 w-200 border border-gray-800 resize-none" 
+              placeholder="Enter Course Description"
+              ></textarea>
             </div>
             <div className="mx-auto w-max p-4 flex flex-col">
               <label className="text-center font-bold text-gray-400 p-2">Upload Course Catalog</label>
-              <input type="file" id="courseCatalog" className="block border border-gray-700 rounded-lg m-2 file:bg-gray-700 file:px-3 file:py-2 file:me-3 file:cursor-pointer file:hover:bg-gray-800"/>
+              <input 
+              type="file" 
+              id="courseCatalog"
+              name="catalog" 
+              accept=".pdf" 
+              className="block border border-gray-700 rounded-lg m-2 file:bg-gray-700 file:px-3 file:py-2 file:me-3 file:cursor-pointer file:hover:bg-gray-800"
+              />
             </div>
-            <button type="submit" className="px-5 py-1.5 m-6 rounded-lg bg-gray-800 mx-auto block text-center cursor-pointer border-gray-700 border text-lg font-bold">Submit</button>
-
+            <button 
+            type="submit" 
+            className="px-5 py-1.5 m-6 rounded-lg bg-gray-800 mx-auto block text-center cursor-pointer border-gray-700 border text-lg font-bold"
+            >Submit</button>
           </form>
-
         </div>
       </div>
     </main>
